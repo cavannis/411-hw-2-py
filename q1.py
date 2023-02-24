60423a1b39ad7a6732e5a025b56d7ae44a392940
cavannis <chappellh@sou.edu>
2023-02-24 12:03:31 -0800

    hw3

---
+++ q1.py
@@ -0,0 +1,39 @@
+import random
+"""merge sort algorithm"""
+
+
+"""insertion sort algorithm"""
+
+
+""" generating the random list of 200 integers below"""
+
+random_list200 = []
+for i in range(200):
+    random_list200.append(random.randint(0, 100))
+
+print(random_list200)
+
+""" generating the random list of 1000 integers below"""
+
+random_list1000 = []
+for i in range(1000):
+    random_list1000.append(random.randint(0, 100))
+
+print(random_list1000)
+
+""" generating the random list of 10000 integers below"""
+
+
+random_list10000 = []
+for i in range(10000):
+    random_list10000.append(random.randint(0, 100))
+
+print(random_list10000)
+
+""" generating the random list of 100000 integers below"""
+
+random_list100000 = []
+for i in range(100000):
+    random_list100000.append(random.randint(0, 100))
+
+print(random_list100000)
